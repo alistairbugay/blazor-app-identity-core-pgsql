fecdd633df866d9c266a32db769a0a3baf41a473
Alistair Bugay <alistairbugay@gmail.com>
2023-12-08 18:13:04 +0800

    initial development codes

---
+++ BlazorApp2/BlazorApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp2.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.UseSerialColumns();
+        }
+
+    }
+}
